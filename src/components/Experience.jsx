ed807bebcdc8276e08109222747a06a95aa20262
Dylan McCaw <dylanmccaw@hotmail.com>
2024-08-07 22:33:45 +0100

    Fixed spacing on mobile

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Title, Text, Grid, Badge, Anchor } from '@mantine/core';
+import { Title, Text, Grid, Badge, Anchor, Box } from '@mantine/core';
 
 function Experience() {
   const experiences = [
@@ -24,41 +24,55 @@ function Experience() {
   ];
 
   return (
-    <>
+    <Box>
       <Title style={{ color: "#248277", paddingBottom: "30px", fontFamily:'Space Mono' }} order={2}>Experience</Title>
-      <Grid gutter="md">
-        {experiences.map((experience, index) => (
-          <React.Fragment key={index}>
-            <Grid.Col span={4} >
-              <Text color="dimmed" size="xs" align="left" style={{ whiteSpace: 'nowrap' }}>
-                {experience.period}
-              </Text>
-            </Grid.Col>
-            <Grid.Col span={8}>
-              <Text weight={700} size="sm">
-                {experience.title} - {' '}
-                <Anchor href={experience.companyLink} target="_blank" style={{ color: '#248277' }}>
-                  {experience.company}
-                </Anchor>
-              </Text>
-              <Text size="xs" style={{ marginTop: '15px'}}>
-                {experience.project}
-              </Text>
-              <Text color="dimmed" size="xs" style={{ marginTop: '20px' }}>
-                {experience.description}
-              </Text>
-              <div style={{ marginTop: '20px', marginBottom: '60px'}}>
-                {experience.skills.map((skill, skillIndex) => (
-                  <Badge key={skillIndex} color="#248277" size="sm" style={{ marginRight: '10px', marginBottom: '10px'}}>
-                    {skill}
-                  </Badge>
-                ))}
-              </div>
-            </Grid.Col>
-          </React.Fragment>
-        ))}
-      </Grid>
-    </>
+      {experiences.map((experience, index) => (
+        <Grid key={index} gutter="md" style={{ marginBottom: '60px' }}>
+          <Grid.Col span={4}>
+            <Text 
+              color="dimmed" 
+              size="xs" 
+              style={{ 
+                maxWidth: '100%', 
+                wordBreak: 'break-word'
+              }}
+            >
+              {experience.period}
+            </Text>
+          </Grid.Col>
+          <Grid.Col span={8}>
+            <Text weight={700} size="sm">
+              {experience.title} - {' '}
+              <Anchor href={experience.companyLink} target="_blank" style={{ color: '#248277' }}>
+                {experience.company}
+              </Anchor>
+            </Text>
+            <Text size="xs" style={{ marginTop: '15px'}}>
+              {experience.project}
+            </Text>
+            <Text color="dimmed" size="xs" style={{ marginTop: '20px' }}>
+              {experience.description}
+            </Text>
+            <Box style={{ marginTop: '20px' }}>
+              {experience.skills.map((skill, skillIndex) => (
+                <Badge 
+                  key={skillIndex} 
+                  color="#248277" 
+                  size="sm" 
+                  style={{ 
+                    marginRight: '10px', 
+                    marginBottom: '10px',
+                    display: 'inline-block'
+                  }}
+                >
+                  {skill}
+                </Badge>
+              ))}
+            </Box>
+          </Grid.Col>
+        </Grid>
+      ))}
+    </Box>
   );
 }
 
